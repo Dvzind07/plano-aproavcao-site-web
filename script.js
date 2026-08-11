c2f93842b2ae1cc1dc110d937c41cd10dba803ae
Dv <rb.davi24@gmail.com>
2026-08-11 19:59:22 -0300

    pronto

---
+++ script.js
@@ -0,0 +1,3 @@
+// Nenhum JavaScript estava presente no arquivo original.
+// Este arquivo foi criado para manter a estrutura de separação
+// (HTML / CSS / JS) e está pronto para receber scripts futuros.
